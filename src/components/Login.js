d5ea12206dee619d088b30889c9d4e51575f5dc9
valeriadomdom <nube@kemtecnia.com>
2026-05-18 13:46:46 +0200

    Fix Login.js sintaxis

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ function Login({ onLogin }) {
   const [error, setError] = useState("");
 
   function handleLogin() {
-   fetch('https://cargadores-servidor-production.up.railway.app/login',
+   fetch('https://cargadores-servidor-production.up.railway.app/login´,{
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
@@ -57,4 +57,4 @@ function Login({ onLogin }) {
   );
 }
 
-export default Login;
+export default Login; 
